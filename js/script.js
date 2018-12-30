33fcaf4f6b1cb52a8b3d14ebbb0e1ea2e1d42625
jfabiantorres <john.timoteo@tecsup.edu.pe>
2018-12-29 19:21:52 -0500

    Carrusel con imagenes usando Slick

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$('.bas').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+    
+$('.withOptions').slick({
+    dots: true,
+    arrows: true,
+    autoplayspeed: 1000,
+    centerMode: true,
+});
---
+++ js/script2.js
@@ -0,0 +1,21 @@
+ $('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav',
+  speed: 200
+   
+});
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: false,
+  centerMode: true,
+  focusOnSelect: true,
+  speed: 200,
+  swipeToSlide: true,
+  infinite: false
+});
+		
